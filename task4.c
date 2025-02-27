1988215cb4a67e81d965846a7d0deb35f5cc5929
Venkata Ande <vande@student.sierracollege.edu>
2025-02-27 14:19:12 -0800

    task 4 'N' argument

--- task4.c
+++ task4.c
@@ -3,18 +3,16 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int main(int argc, char *argv[]) 
-{
-    if (argc != 3) 
+int main(int argc, char *argv[]) {
+    if (argc != 4) 
     {
-        printf("Usage: %s <program> <file>\n", argv[0]);
+        printf("Usage: %s <program> <file> <N>\n", argv[0]);
         return 1;
     }
 
     // Open the file for writing (create if it doesn't exist)
     int fd = open(argv[2], O_WRONLY | O_CREAT, 0644);
-    if (fd < 0) 
-    {
+    if (fd < 0) {
         perror("open failed");
         return 1;
     }
@@ -27,13 +25,13 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Close the original file descriptor (not needed anymore)
+    // Close the OG file descriptor
     close(fd);
 
-    // Execute the specified program
-    execl(argv[1], argv[1], NULL);
+    // Execute the  program
+    execl(argv[1], argv[1], argv[3], NULL);
 
-    // If exec fails, print an error and return
+    // If exec fails
     perror("execl failed");
     return 1;
 }
